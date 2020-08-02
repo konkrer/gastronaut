a01911f3818e968be5b9b023dc2cd1dc7467e39e
konkrer <rivortex@gmail.com>
2020-08-01 22:27:01 -0700

    objectified all .js except index.js

--- static/js/base.js
+++ static/js/base.js
@@ -1,65 +1,88 @@
 'use strict';
 
-let preferencesTimer;
-let feedbackClearTimer;
+// Basic logic common to all pages.
+class BaseLogic {
+  constructor() {
+    this.preferencesTimer;
+    this.feedbackClearTimer;
+    this.addPreferencesListener();
+    this.addAlertCloseListener();
+  }
 
-/*
-/* Set Preferences listener
-*/
-$('#preferences-form').on('change', 'input', () => {
-  clearTimeout(preferencesTimer);
-  preferencesTimer = setTimeout(updatePreferences, 400);
-});
+  // User Preferences listener
+  addPreferencesListener() {
+    $('#preferences-form').on('change', 'input', () => {
+      clearTimeout(this.preferencesTimer);
+      this.preferencesTimer = setTimeout(
+        this.updatePreferences.bind(this),
+        400
+      );
+    });
+  }
 
-/*
-/* Call API set_preferences endpoint.
-*/
-async function updatePreferences() {
-  const data = convertDataArrayToObj($('#preferences-form').serializeArray());
-  const resp = await axios.post('/v1/preferences', data);
+  // Call API set_preferences endpoint.
 
-  $('#preferencesModal .feedback').text(resp.data.feedback);
-  clearTimeout(feedbackClearTimer);
-  feedbackClearTimer = setTimeout(() => {
-    $('#preferencesModal .feedback').text('');
-  }, 2000);
+  async updatePreferences() {
+    const data = this.convertDataArrayToObj(
+      $('#preferences-form').serializeArray()
+    );
+    const resp = await axios.post('/v1/preferences', data);
+
+    $('#preferencesModal .feedback').text(resp.data.feedback);
+    clearTimeout(this.feedbackClearTimer);
+    this.feedbackClearTimer = setTimeout(() => {
+      $('#preferencesModal .feedback').text('');
+    }, 2000);
+  }
+
+  // Enable service worker for PWA.
+  // Pulled from Google docs about service workers.
+  enableServiceWorker() {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/static/js/sw.js').then(
+          function (registration) {
+            // Registration was successful
+            console.log(
+              'ServiceWorker registration successful with scope: ',
+              registration.scope
+            );
+          },
+          function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+          }
+        );
+      });
+    }
+  }
+
+  /*
+  /* Close Alert (flash) function.
+  */
+  addAlertCloseListener() {
+    $('.alert-close').click(() => {
+      $('.alert').remove();
+    });
+  }
+
+  /*
+  /* Convert the array of objects that jQuery returns from serializeArray
+  /* to a single object with key value pairs for all data.
+  */
+  convertDataArrayToObj(data) {
+    // reduce data from array of objects to obj.
+    return data.reduce((acc, curr) => {
+      acc[curr.name] = curr.value;
+      return acc;
+    }, {});
+  }
 }
 
-// Enable service worker for PWA.
-// Pulled from Google docs about service workers.
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/static/js/sw.js').then(
-      function (registration) {
-        // Registration was successful
-        console.log(
-          'ServiceWorker registration successful with scope: ',
-          registration.scope
-        );
-      },
-      function (err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      }
-    );
+const Base = new BaseLogic();
+
+function sleep(delay) {
+  return new Promise(resolve => {
+    setTimeout(resolve, delay);
   });
 }
-
-/*
-/* Convert the array of objects that jQuery returns from serializeArray
-/* to a single object with key value pairs for all data.
-*/
-function convertDataArrayToObj(data) {
-  // reduce data from array of objects to obj.
-  return data.reduce((acc, curr) => {
-    acc[curr.name] = curr.value;
-    return acc;
-  }, {});
-}
-
-/*
-/* Close Alert (flash) function.
-*/
-$('.alert-close').click(() => {
-  $('.alert').remove();
-});
