061ad14b1c1546ca81b05514f4a5a07516dc978e
konkrer <rivortex@gmail.com>
2020-08-12 20:34:35 -0700

    wake lock sentry messages debugging

--- static/js/geolocate.js
+++ static/js/geolocate.js
@@ -113,11 +113,16 @@ class GeolocationObj {
         if (this.wakeLock === null) {
           try {
             this.wakeLock = await navigator.wakeLock.request('screen');
+            Sentry.captureMessage(
+              `wakelock is object: ${typeof this.wakeLock === 'object'}`
+            );
           } catch (err) {
+            Sentry.captureException(err);
+            Sentry.captureMessage(`wakelock error`);
             console.warn('ERROR(' + err.code + '): ' + err.message);
           }
         }
-      }
+      } else Sentry.captureMessage('No wakelock in navigator!');
     }
   }
 
